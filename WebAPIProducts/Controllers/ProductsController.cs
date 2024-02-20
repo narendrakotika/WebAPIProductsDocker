9db4d36b94e411f1d477ab1c21939f7ef8fcb8cc
Narendra <narendrakotika3576@gmail.com>
2024-02-20 15:35:52 +0530

    Add project files.

---
+++ WebAPIProducts/Controllers/ProductsController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebAPIProducts.Models;
+
+namespace WebAPIProducts.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private static readonly List<Product> products = new List<Product>()
+        {
+            new() {PId=1, PName="Herbal Shampoo", Price=500},
+
+            new() {PId=2, PName="Face Mask", Price=700},
+
+           new() {PId=3, PName="Face Wash", Price=700},
+
+        new() {PId=5,PName="Glow & Shine", Price=300},
+
+     new() {PId=6, PName="Anti Aging Cream", Price=890},
+
+        };
+
+
+        [HttpGet(Name = "GetProducts")]
+
+        public IEnumerable<Product> Get()
+        { 
+
+return products;
+}
+
+
+    }
+}
---
+++ WebAPIProducts/Models/Product.cs
@@ -0,0 +1,9 @@
+﻿namespace WebAPIProducts.Models
+{
+    public class Product
+    {
+        public int PId { get; set; }
+        public string PName { get; set; }
+        public double Price { get; set; }
+    }
+}
